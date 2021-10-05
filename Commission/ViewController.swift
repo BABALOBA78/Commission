0cf948282a67d608bf58a5ecdd8b487232ea7aed
nicholas deaconu <nicholasdeaconu5072@stu.d214.org>
2021-10-05 10:43:42 -0700

    Final Commision Pay

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    @IBOutlet weak var CommissionPayTextField: UILabel!
+    @IBOutlet weak var BasePayLabel: UILabel!
+    
+    let basePay:Double = 500
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func WhenButtonPressed(_ sender: Any) {
+        let dataString = CommissionPayTextField.text!
+        let commisionPay = Double(dataString) ?? 5.0
+        let totalPay = basePay + commisionPay
+        TotalPayLabel.text = " $\(totalPay)"
+    }
+    
 }
 
